dc4a1716b90bdae673f0fd08a5aa9f05d282c8de
Anand Raj Bharti <anandrajbharti574@gmail.com>
2026-04-01 17:43:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let div = document.getELementById("output");
+
+
+let p = new Promise((res, rej)=>{
+	setTimeout(()=>{
+		res "Hello, world"
+	}, 1000);
+})
+
+div.innerText = p;
